6ceca92f7f8c8c563a69b1a7707ec55f3656e967
Nathan <nlaha@outlook.com>
2023-08-25 11:54:01 -0700

    update

--- run.ps1
+++ run.ps1
@@ -1,4 +1,3 @@
-scons platform=windows target=template_debug debug_symbols=yes;
+scons platform=windows target=template_debug;
 cp .\bin\libgodotsteam.windows.template_debug.x86_64.dll ..\godot-FirstPersonStarter-main\addons\godotsteam\win64\godotsteam.debug.x86_64.dll;
-cp .\bin\libgodotsteam.windows.template_debug.x86_64.pdb ..\godot-FirstPersonStarter-main\addons\godotsteam\win64\godotsteam.debug.x86_64.pdb;
 godot -e --path ..\godot-FirstPersonStarter-main\
